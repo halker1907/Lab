70041d1acd1528d6d437c7773244cf90bb4e2385
halker1907 <113712157+halker1907@users.noreply.github.com>
2024-05-18 14:48:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
+import random
+import os
 CANVAS_BG = 'red'
+COLS = 25
+ROWS = 15
+
+WALL = '▓'
+EMPTY = '░'
+maze = []
+
 """
 1.список списка или список строк
 2.Полностью заполнить лабиринт стенами
@@ -8,20 +17,54 @@ CANVAS_BG = 'red'
 5.если в двух клетках от него стены - ломает обе
 6.когда во всех четных клетках нет стен лабиринт проходим
 7.надо по границам обрисовать стеной, и сделать вход с выходом
-    '░'
 """
-window = tk.Tk()
-window.geometry("250x200")
-canvas = tk.Canvas(
-    window,
-    width=250,
-    height=200,
-    bg=CANVAS_BG,
-    highlightthickness=0
-)
-canvas.pack(expand=True)
-canvas.update()
-row = [
-    '▓',
-]
-window.mainloop()
+for row_idx in range(ROWS):
+    row = []
+    for col in range(COLS):
+        row.append(WALL)
+    maze.append(row)
+
+buldozer_col = random.choice(range(0, COLS, 2))
+buldozer_row = random.choice(range(0, ROWS, 2))
+maze[buldozer_row][buldozer_col] = '@'
+
+for i in range(100):
+    buldozer_direction = []
+    if buldozer_col + 2 < COLS:
+        buldozer_direction.append('right')
+    if buldozer_col - 2 >= 0:
+        buldozer_direction.append('left')
+    if buldozer_row + 2 < ROWS:
+        buldozer_direction.append('down')
+    if buldozer_row - 2 >= 0:
+        buldozer_direction.append('up')
+
+    if not buldozer_direction:
+        print('опа')
+        break
+
+    direction = random.choice(buldozer_direction)
+    if direction == 'right':
+        if maze[buldozer_row][buldozer_col + 2] == WALL:
+            maze[buldozer_row][buldozer_col + 1] = EMPTY
+            maze[buldozer_row][buldozer_col + 2] = EMPTY
+        buldozer_col += 2
+    elif direction == 'left':
+        if maze[buldozer_row][buldozer_col - 2] == WALL:
+            maze[buldozer_row][buldozer_col - 1] = EMPTY
+            maze[buldozer_row][buldozer_col - 2] = EMPTY
+        buldozer_col -= 2
+    elif direction == 'up':
+        if maze[buldozer_row - 2][buldozer_col] == WALL:
+            maze[buldozer_row - 1][buldozer_col] = EMPTY
+            maze[buldozer_row - 2][buldozer_col] = EMPTY
+        buldozer_row -= 2
+    elif direction == 'down':
+        if maze[buldozer_row + 2][buldozer_col] == WALL:
+            maze[buldozer_row + 1][buldozer_col] = EMPTY
+            maze[buldozer_row + 2][buldozer_col] = EMPTY
+        buldozer_row += 2
+
+    os.system('cls')
+    for row in maze:
+        print(*row, sep='')
