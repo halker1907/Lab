c01f7c54d2745432dccfa628a89c72f429906707
halker1907 <113712157+halker1907@users.noreply.github.com>
2024-05-29 19:47:14 +0300

    Add files via upload

--- app_ganerate.py
+++ app_ganerate.py
@@ -38,58 +38,59 @@ def make_borders():
 
 #нулевой ряд и предпоследняя колонна
 """
-for row_idx in range(ROWS):
-    row = []
-    for col in range(COLS):
-        row.append(WALL)
-    maze.append(row)
+def generate_maze(rows=27, cols=51):
+    for row_idx in range(ROWS):
+        row = []
+        for col in range(COLS):
+            row.append(WALL)
+        maze.append(row)
 
-# Set the entrance at the first row
+    # Set the entrance at the first row
 
-buldozer_col = random.choice(range(0, COLS, 2))
-buldozer_row = random.choice(range(0, ROWS, 2))
-maze[buldozer_row][buldozer_col] = '@'
+    buldozer_col = random.choice(range(0, COLS, 2))
+    buldozer_row = random.choice(range(0, ROWS, 2))
 
-while True:
-    buldozer_direction = []
-    if buldozer_col + 2 < COLS:
-        buldozer_direction.append('right')
-    if buldozer_col - 2 >= 0:
-        buldozer_direction.append('left')
-    if buldozer_row + 2 < ROWS:
-        buldozer_direction.append('down')
-    if buldozer_row - 2 >= 0:
-        buldozer_direction.append('up')
+    while True:
+        buldozer_direction = []
+        if buldozer_col + 2 < COLS:
+            buldozer_direction.append('right')
+        if buldozer_col - 2 >= 0:
+            buldozer_direction.append('left')
+        if buldozer_row + 2 < ROWS:
+            buldozer_direction.append('down')
+        if buldozer_row - 2 >= 0:
+            buldozer_direction.append('up')
 
-    if not buldozer_direction:
-        print('нет свободных направлений')
-        break
+        if not buldozer_direction:
+            print('нет свободных направлений')
+            break
 
-    direction = random.choice(buldozer_direction)
-    if direction == 'right':
-        if maze[buldozer_row][buldozer_col + 2] == WALL:
-            maze[buldozer_row][buldozer_col + 1] = EMPTY
-            maze[buldozer_row][buldozer_col + 2] = EMPTY
-        buldozer_col += 2
-    elif direction == 'left':
-        if maze[buldozer_row][buldozer_col - 2] == WALL:
-            maze[buldozer_row][buldozer_col - 1] = EMPTY
-            maze[buldozer_row][buldozer_col - 2] = EMPTY
-        buldozer_col -= 2
-    elif direction == 'up':
-        if maze[buldozer_row - 2][buldozer_col] == WALL:
-            maze[buldozer_row - 1][buldozer_col] = EMPTY
-            maze[buldozer_row - 2][buldozer_col] = EMPTY
-        buldozer_row -= 2
-    elif direction == 'down':
-        if maze[buldozer_row + 2][buldozer_col] == WALL:
-            maze[buldozer_row + 1][buldozer_col] = EMPTY
-            maze[buldozer_row + 2][buldozer_col] = EMPTY
-        buldozer_row += 2
+        direction = random.choice(buldozer_direction)
+        if direction == 'right':
+            if maze[buldozer_row][buldozer_col + 2] == WALL:
+                maze[buldozer_row][buldozer_col + 1] = EMPTY
+                maze[buldozer_row][buldozer_col + 2] = EMPTY
+            buldozer_col += 2
+        elif direction == 'left':
+            if maze[buldozer_row][buldozer_col - 2] == WALL:
+                maze[buldozer_row][buldozer_col - 1] = EMPTY
+                maze[buldozer_row][buldozer_col - 2] = EMPTY
+            buldozer_col -= 2
+        elif direction == 'up':
+            if maze[buldozer_row - 2][buldozer_col] == WALL:
+                maze[buldozer_row - 1][buldozer_col] = EMPTY
+                maze[buldozer_row - 2][buldozer_col] = EMPTY
+            buldozer_row -= 2
+        elif direction == 'down':
+            if maze[buldozer_row + 2][buldozer_col] == WALL:
+                maze[buldozer_row + 1][buldozer_col] = EMPTY
+                maze[buldozer_row + 2][buldozer_col] = EMPTY
+            buldozer_row += 2
 
-    if is_passable():
-        break
+        if is_passable():
+            break
 
-make_borders()
-maze[0][COLS] = EXIT
-draw_lab()
+    make_borders()
+    maze[0][COLS] = EXIT
+    draw_lab()
+    return maze
--- main.py
+++ main.py
@@ -1,36 +1,56 @@
 import tkinter as tk
 import app_ganerate
 CANVAS_BG = 'red'
-WINDOW_BG = 'yellow'
+WINDOW_BG = 'black'
+COLOR_PLAYER = 'yellow'
 EMPTY = '█'
 WALL = '░'
 EXIT = 'X'
 MAZE_COLORS = {
-    WALL = 'black'
-    EXIT = 'red'
-    EMPTY = 'green'
+    WALL: 'black',
+    EXIT: 'red',
+    EMPTY: 'green',
 }
-TODO: #сделать в др файле штуку которая будет делать мейз generate_maze(COLS=self.cols, ROWS=self.cols)
 
 class App():
-    def __init__(self):
+    def __init__(self, cols, rows):
         self.window = tk.Tk()
-        self.cols = 51
-        self.rows = 27
-        self.tile_size = 40
-        self.maze = app_ganerate.generate_maze(COLS=self.cols, ROWS=self.cols)
-        self.window.attributes('-fullscreen', True) 
+        self.cols = cols
+        self.rows = rows
+        self.min_side = min(self.window.winfo_screenwidth() // self.cols, self.window.winfo_screenheight() // self.rows) 
+        self.tile_size = self.min_side // max(cols, rows)
+        self.maze = None
+        self.player = None
+        self.is_running = False
+        self.window.attributes('-fullscreen', True, bg=WINDOW_BG) 
         self.window.bind('<Key>', self.on_key)
         self.canvas = tk.Canvas(
-            self.window, highlightthickness=0)
+            self.window,
+            width=self.cols * self.tile_size,
+            height=self.rows * self.tile_size,
+            highlightthickness=0
+            )
         self.canvas.pack(expand=True, fill='both')
         self.draw_maze()
         self.window.mainloop()
     
     def draw_maze(self):
         for row_idx, row in enumerate(self.maze):#энумерейт возвращяет кортежи
-            for col_idx, col in enumerate(self.row):
-                self.canvas.create_rectangle(col_idx * self.tile_size + self.tile_size, row_idx * self.tile_size, col_idx * self.tile_size + self.tile_size, row_idx * self.tile_size, fill=MAZE_COLORS[col])
+            for col_idx, col in enumerate(row):
+                self.canvas.create_rectangle(
+                    col_idx * self.tile_size,
+                    row_idx * self.tile_size,
+                    col_idx * self.tile_size + self.tile_size,
+                    row_idx * self.tile_size + self.tile_size,
+                    fill=MAZE_COLORS[col]
+                    )
+        
+    def start(self):
+        self.maze = app_ganerate.generate_maze(cols=self.cols, rows=self.cols)
+        self.draw_maze()
+        self.player = Player(self, self.rows - 2, 1, self.tile_size, COLOR_PLAYER)
+        self.is_running = True
+
 
     def on_key(self, event: tk.Event) -> None:
         if event.keysym == 'Escape':
@@ -38,3 +58,26 @@ class App():
 '''
 TODO во втором файле сделать GUI 
 '''
+class Player:
+    def __init__(self, game: App, row: int, col: int, size: int, color: str) -> None:
+        self.game = game
+        self.col = col
+        self.row = row
+        self.size = size
+        self.color = color
+        self.draw()
+
+
+
+    def draw(self):
+        self.game.canvas.create_rectangle(
+            self.col * self.game.tile_size,
+            self.row * self.game.tile_size,
+            self.col * self.game.tile_size + self.game.tile_size,
+            self.row * self.game.tile_size + self.game.tile_size,
+            fill=self.color
+        )
+    def move(self):
+        pass
+            
+App(51, 27)
